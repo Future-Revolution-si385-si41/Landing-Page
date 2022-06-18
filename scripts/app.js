874601d7389442275552ad0d633e0f02e4f36f63
Djalma Dioses <54141599+djalmadioses@users.noreply.github.com>
2022-06-17 21:49:59 -0500

    feat: base proyect

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+function scrollNavegation (){
+    const enlaces = document.querySelectorAll(".header-menu a");
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click',e => {
+            e.preventDefault();
+            const seccion = document.querySelector(e.target.attributes.href.value);
+            seccion.scrollIntoView({ behavior: "smooth" });
+        });
+        
+    });
+    document.querySelector(".presentacion-ingresar a").addEventListener('click', e => {
+        e.preventDefault();
+        const seccion = document.querySelector(e.target.attributes.href.value);
+        seccion.scrollIntoView({ behavior: "smooth" });
+    })
+}
+scrollNavegation();
